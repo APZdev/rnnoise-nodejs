eb88c5075f4f9f5885f04c2c065030105fdbad77
APZdev <difallahadam2003@gmail.com>
2024-06-08 21:27:55 +0200

    - Downgraded from version 0.1.8 to 0.0.1 - Added package typings - Changed binaries directory to APZdev

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+declare module "rnnoise-fork" {
+  /**
+   * Suppress noise in an audio file.
+   *
+   * @param inputPath - The path to the input audio file.
+   * @param outputPath - The path to the output audio file where the denoised audio will be saved.
+   * @returns The length of the denoised buffer.
+   */
+  export function suppress(inputPath: string, outputPath: string): number;
+}
